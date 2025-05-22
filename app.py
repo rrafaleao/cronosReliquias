7c8725bbda341fe58c54dc015bf815a641b674ca
RAFAEL CAMPERA DE LEAO <05163643088@senacrs.edu.br>
2025-05-22 20:29:13 -0300

    teste

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -6,4 +7,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Usa a porta do Render, ou 5000 como fallback local
+    app.run(host="0.0.0.0", port=port)
