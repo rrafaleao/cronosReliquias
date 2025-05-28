e388123a135d8f8ae1de3eccc374f776cdcd00d3
unknown <rafaleao2007@gmail.com>
2025-05-28 18:37:17 -0300

    Atualizações

--- old/routes/main_routes.py
+++ old/routes/main_routes.py
@@ -2,6 +2,14 @@ from flask import Blueprint, render_template, flash, redirect, url_for, session,
 
 main = Blueprint('main', __name__)
 
-@main.route('/teste')
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/sobre')
 def teste():
-    return render_template('teste.html')
+    return render_template('about.html')
+
+@main.route('/catalogo')
+def catalogo():
+    return render_template('catalog.html')
